76dfe69b95b59fe3eeab9339713baf9b980336cd
codewithamitpatil <62344675+codewithamitpatil@users.noreply.github.com>
2021-05-28 20:27:01 +0530

    Update init_redis.js

--- config/init_redis.js
+++ config/init_redis.js
@@ -1,12 +1,14 @@
 
-const redis    = require('redis');
-const url = require("url");
+const redis = require('redis');
 
-const client = redis.createClient(new url.URL(process.env.REDISTOGO_URL));
-
+const client = redis.createClient({
+  port:17140,
+  auth_pass:'61JlUUpoNN3t7m5KI7XhrSSmRaXVPmEE',
+  host:'redis-17140.c245.us-east-1-3.ec2.cloud.redislabs.com'
+});
 
 client.on('connect',async()=>{
-  console.log('redis connected 11');
+  console.log('redis connected');
 });
 
 client.on('error',async(err)=>{
@@ -14,7 +16,7 @@ client.on('error',async(err)=>{
 });
 
 client.on('end',async()=>{
-  console.log('redis end 111');
+  console.log('redis end');
 });
 
 process.on('SIGINT',()=>{
