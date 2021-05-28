cd3cf880302431609c36ec374784506be565453c
codewithamitpatil <62344675+codewithamitpatil@users.noreply.github.com>
2021-05-28 17:51:25 +0530

    Update init_redis.js

--- config/init_redis.js
+++ config/init_redis.js
@@ -1,11 +1,12 @@
 
 const redis    = require('redis');
+const url = require("url");
 
 let client;
 
 if (process.env.REDISTOGO_URL) {
  console.log('///////////');
-var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+var rtg   = new url.URL(process.env.REDISTOGO_URL);
 client = require("redis").createClient(rtg.port, rtg.hostname);
 
 client.auth(rtg.auth.split(":")[1]);
